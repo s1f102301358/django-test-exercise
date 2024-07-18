b82ed25ab6251f175ce2a22243e1b0be2cc3da35
1F10230135 <s1f102301358@iniad.org>
2024-07-18 11:37:15 +0900

    テストケースの修正

--- todo/tests.py
+++ todo/tests.py
@@ -71,6 +71,9 @@ class TodoViewTestCase(TestCase):
         data = {'title': 'Task Task', 'due_at': '2024-06-30 23:59:59'}
         response = client.post('/', data)
 
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/')
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 1)
